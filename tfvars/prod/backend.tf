678338619028a776aae2f2cd2e97d65e9a553270
GattamPavan <pavanreddy2266@gmail.com>
2026-05-27 22:33:37 +0530

    terraform

--- tfvars/prod/backend.tf
+++ tfvars/prod/backend.tf
@@ -1,5 +1,9 @@
-bucket = "roboshop-88s-prod"
-key    = "roboshop.tfstate"
-region = "us-east-1"
-encrypt = true
-use_lockfile = true
+terraform {
+  backend "s3" {
+    bucket         = "roboshop-88s-prod"
+    key            = "roboshop.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile   = true
+  }
+}
